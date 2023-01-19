8904e074afeb1027adc7e3ee4475a070f9428b12
Kender ROMAIN <kenderromain@gmail.com>
2023-01-19 16:53:08 -0400

    Sort the list items differently every time we refresh the page

--- js/app.js
+++ js/app.js
@@ -2,44 +2,48 @@
 const draggableList = document.querySelector('#draggable-list');
 
 // Table for the check
-const richestPeople = [
-  'Jeff Bezos',
-  'Bill Gates',
-  'Warren Buffett',
-  'Bernard Arnault',
-  'Carlos Slim Helu',
-  'Amancio Ortega',
-  'Larry Ellison',
-  'Mark Zuckerberg',
-  'Michael Bloomberg',
-  'Larry Page',
+const highestMountains = [
+  'Mount Everest',
+  'K2',
+  'Kangchenjunga',
+  'Lhotse',
+  'Makalu',
+  'Cho Oyu',
+  'Dhaulagiri I',
+  'Manaslu',
+  'Nanga Parbat',
+  'Annapurna I',
 ];
 
-// Our list items pure HTML elements
+// Our list items pure HTML elements. Like a reference
 const listItems = [];
 // Where the drag starts
 let dragStartIndex;
 
 // Generate the list into DOM
 const generateList = () => {
-  [...richestPeople].map((people, index) => {
-    // Create an li
-    const li = document.createElement('li');
-    // Create a custom attribute
-    li.setAttribute('data-index', index);
-    // Add elements for the listItem
-    li.innerHTML = `
+  [...highestMountains]
+    .map((val) => ({ name: val, sort: Math.random() })) // In order to render the list ramdomly
+    .sort((a, b) => a.sort - b.sort)
+    .forEach((mountain, index) => {
+      console.log(mountain.name);
+      // Create an li
+      const li = document.createElement('li');
+      // Create a custom attribute
+      li.setAttribute('data-index', index);
+      // Add elements for the listItem
+      li.innerHTML = `
     <span class="position">${index + 1}</span>
     <div class="draggable" draggable="true" >
-      <p class="people-name">${people}</p>
+      <p class="mountain-name">${mountain.name}</p>
       <span class="material-symbols-outlined">more_vert</span>
     </div>
     `;
-    // Push my item in the listItem
-    listItems.push(li);
-    // Add it into DOM (draggableList)
-    draggableList.appendChild(li);
-  });
+      // Push my item in the listItem
+      listItems.push(li);
+      // Add it into DOM (draggableList)
+      draggableList.appendChild(li);
+    });
 };
 
 generateList();
